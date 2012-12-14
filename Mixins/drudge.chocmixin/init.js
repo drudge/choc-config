a1eb3d5651ab55d33295749a882c869bb4331b2a
Nicholas Penree <nick@penree.com>
2012-12-14 15:36:51 -0500

    Add ability to open Chocolat App support folder

--- Mixins/drudge.chocmixin/init.js
+++ Mixins/drudge.chocmixin/init.js
@@ -1,7 +1,15 @@
 /**
+ * Module dependencies.
+ */
+
+var exec = require('child_process').exec;
+var path = require('path');
+var fs = require('fs');
+
+/**
  * Override keyboard shortcuts.
  */
- 
+
 // "Fix" Go To file...
 Hooks.setShortcutForMenuItem("Go/Go to File…", "cmd-t"); //…
 Hooks.setShortcutForMenuItem("File/New Workspace", "cmd-option-t"); //…
@@ -9,3 +17,7 @@ Hooks.setShortcutForMenuItem("File/New Workspace", "cmd-option-t"); //…
 // "Fix" command-n
 Hooks.setShortcutForMenuItem("File/New Window", "cmd-n");
 Hooks.setShortcutForMenuItem("File/New File", "cmd-shift-n");
+
+Hooks.addMenuItem('Actions/Chocolat/Open App Support Folder…', '', function() {
+  exec('open "' + path.join(process.env.HOME, "Library", "Application Support", "Chocolat") + '"');
+});
