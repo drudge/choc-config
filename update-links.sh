6c83aaba3d42f6c2a5a6c89b7c06681a178b4418
Nicholas Penree <nick@penree.com>
2013-06-26 22:24:10 -0400

    Update update-links.sh

--- update-links.sh
+++ update-links.sh
@@ -5,14 +5,14 @@ APP_SUPPORT="$HOME/Library/Application Support/Chocolat"
 
 echo "Linking completions..."
 
-rm "$CHOCOLAT_APP/Contents/Resources/completions.json"
-ln -s "$APP_SUPPORT/completions.json" "$CHOCOLAT_APP/Contents/Resources/completions.json"
+#rm "$CHOCOLAT_APP/Contents/Resources/completions.json"
+#ln -s "$APP_SUPPORT/completions.json" "$CHOCOLAT_APP/Contents/Resources/completions.json"
 
 echo "Linking templates..."
 
 rm "$CHOCOLAT_APP/Contents/Resources/boilerplate.json"
 ln -s "$APP_SUPPORT/boilerplate.json" "$CHOCOLAT_APP/Contents/Resources/boilerplate.json"
 
-echo "Removing built-in JavaScript truffle..."
+#echo "Removing built-in JavaScript truffle..."
 
-rm -rf "$CHOCOLAT_APP/Contents/SharedSupport/Truffles/javascript.truffle"
+#rm -rf "$CHOCOLAT_APP/Contents/SharedSupport/Truffles/javascript.truffle"
